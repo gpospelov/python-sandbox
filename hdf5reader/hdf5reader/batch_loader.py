2d9fc5c4f464d4033dd5280ae7f4c86f542eb6b3
Gennady Pospelov <g.pospelov@fz-juelich.de>
2019-09-19 11:43:01 +0200

    Add more performance tests for data loaders.

--- hdf5reader/hdf5reader/batch_loader.py
+++ hdf5reader/hdf5reader/batch_loader.py
@@ -4,41 +4,38 @@ BatchLoader class: load hdf5 data in multi processing mode.
 from multiprocessing import cpu_count
 from hdf5reader.file_finder import FileFinder
 from hdf5reader.dataset_finder import DatasetFinder
+from hdf5reader.dataset_finder import DatasetFinderV2
 from hdf5reader.load_h5view_data import load_h5view_data
 import time
+
 
 class BatchLoader:
     def __init__(self, files, batch_size=32):
         self.batch_size = batch_size
-        dataset_finder = DatasetFinder(files)
+        dataset_finder = DatasetFinderV2(files)
         self.datasets = dataset_finder.get_datasets()
         self.idx = list(range(len(self.datasets)))
         self.steps_per_epoch = len(self.idx) // self.batch_size
-        self.x = None
-        self.y = None
 
     def get_batch(self, batch_id):
         start_index = batch_id*self.batch_size
         end_index = start_index + self.batch_size
         # print("get_batch {} {}".format(start_index, end_index))
         views_to_load = self.datasets[start_index:end_index]
-        self.x, self.y = load_h5view_data(views_to_load)
-        pass
+        return load_h5view_data(views_to_load)
 
     def __len__(self):
         return self.steps_per_epoch
 
 
-if __name__ == '__main__':
-    n_files = 90
-    batch_size = 32
-
-    file_finder = FileFinder("/mnt/space1/pospelov/data/d3hack/ver1/hdf5/datam_*.h5", n_files)
-    print(file_finder.get_files())
-
+def test_batch_loader(batch_loader_class, file_pattern, nfiles, batch_size):
+    file_finder = FileFinder(file_pattern, nfiles)
 
     start = time.time()
-    loader = BatchLoader(file_finder.get_files(), batch_size)
+    print("Constructing batch loader for {} files ...".format(len(file_finder.get_files())))
+    loader = batch_loader_class(file_finder.get_files(), batch_size)
+    print("... done in {:.3f}. Steps per epoch {}.".format(time.time() - start, len(loader)))
+
     print("Starting data loader for {} batches".format(len(loader)))
     for i in range(len(loader)):
         if i%10 == 0:
@@ -48,4 +45,7 @@ if __name__ == '__main__':
     print("... done in {:.3f}. batch_size {}, n_batches {}, n_files {}."
           .format(time.time() - start, batch_size, len(loader), len(file_finder.get_files())))
 
+
+if __name__ == '__main__':
+    test_batch_loader(BatchLoader, "/mnt/space2/pospelov/data/d3hack/ver1/hdf5_v2/datam_*.h5", 20, 128)
 
--- hdf5reader/hdf5reader/load_h5view_data.py
+++ hdf5reader/hdf5reader/load_h5view_data.py
@@ -3,37 +3,36 @@ Collection of functions to load/process hdf5 dataset data
 """
 import numpy as np
 from hdf5reader.dataset_finder import DatasetFinder
+from hdf5reader.dataset_finder import DatasetFinderV2
 from hdf5reader.file_finder import FileFinder
 import time
 
 
 def load_h5view_data(dataset_views):
-    # images = [np.ndarray(x.shape, x.dtype) for x in dataset_views]
-    # labels = [dict(x.attrs) for x in dataset_views]
+    images = [np.ndarray(x.shape, x.dtype) for x in dataset_views]
+    labels = [dict(x.attrs) for x in dataset_views]
 
-    # for i, view in enumerate(dataset_views):
-    #     view.read_direct(images[i])
-    images = []
     for i, view in enumerate(dataset_views):
-        images.append(np.array(view))
+        view.read_direct(images[i])
+    # images = []
+    # for i, view in enumerate(dataset_views):
+    #     images.append(np.array(view))
 
-    return images
+    return images, labels
 
 
-if __name__ == '__main__':
-    file_finder = FileFinder("/mnt/space1/pospelov/data/d3hack/ver1/hdf5/datam_*.h5", 20)
+def test_load_data(dataset_finder_class, file_pattern, maxfiles):
+    file_finder = FileFinder(file_pattern, maxfiles)
 
     print("Constructing dataset...")
     start = time.time()
-    datasets = DatasetFinder(file_finder.get_files())
+    datasets = dataset_finder_class(file_finder.get_files())
     print("... done in {:.3f}.".format(time.time() - start))
 
     print("Loading views...")
     start = time.time()
     views = datasets.get_datasets()
     print("... done in {:.3f}. Total views {}.".format(time.time() - start, len(views)))
-
-    # label = np.array([raw_data.attrs[param] for param in self.parameters])
 
     print("Processing images in batches...")
     start = time.time()
@@ -44,7 +43,12 @@ if __name__ == '__main__':
         end_index = start_index + batch_size
         print("batch_id {}/{}, start_index {}, end_index {}"
               .format(batch_id, n_batches, start_index, end_index))
-        images = load_h5view_data(views[start_index:end_index])
+        images, labels = load_h5view_data(views[start_index:end_index])
         print(len(images), images[0].dtype, type(images[0]))
 
     print("... done in {:.3f}.".format(time.time() - start))
+
+
+if __name__ == '__main__':
+    test_load_data(DatasetFinder, "/mnt/space2/pospelov/data/d3hack/ver1/hdf5/datam_*.h5", 20)
+    # test_load_data(DatasetFinderV2, "/mnt/space2/pospelov/data/d3hack/ver1/hdf5_v2/datam_*.h5", 20)
